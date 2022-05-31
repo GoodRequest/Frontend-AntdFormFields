a9127c3ffaebadab67d1fd5056c0e433ca0e67c4
HaluskaRoman <roman.haluska@goodrequest.com>
2022-05-31 16:12:29 +0200

    fixing issues

--- src/commands/cypressCommands.tsx
+++ src/commands/cypressCommands.tsx
@@ -1,35 +1,34 @@
 import 'cypress-file-upload'
+import 'cypress-localstorage-commands'
 
 import { generateElementId } from '../utils/helper'
 
-export const apiAuth = (email: string, password: string, url: string) => {
+Cypress.Commands.add('apiAuth', (email: string, password: string) => {
     cy.log(`Login as ${email}`)
     cy.request({
         method: 'POST',
-        url: url,
+        url: '/api/b2b/admin/auth/login',
         body: {
             email,
             password
         }
-    }).then(({ body }: any) => {
+    }).then(({ body }) => {
         window.localStorage.setItem('access_token', body.accessToken)
-        if (body.refreshToken) {
-            window.localStorage.setItem('refresh_token', body.refreshToken)
-        }
+        window.localStorage.setItem('refresh_token', body.refreshToken)
         cy.saveLocalStorage()
     })
-}
+})
 
-export const setInputValue = (form: string, key: string, value: string, clear?: boolean) => {
+Cypress.Commands.add('setInputValue', (form: string, key: string, value: string, clear?: boolean) => {
     const elementId: string = generateElementId(key, form)
     if (clear) {
         cy.get(elementId).clear().type(value).should('have.value', value)
     } else {
         cy.get(elementId).type(value).should('have.value', value)
     }
-}
+})
 
-export const selectOptionDropdown = (form: string, key: string, value?: string) => {
+Cypress.Commands.add('selectOptionDropdown', (form: string, key: string, value?: string) => {
     const elementId: string = generateElementId(key, form)
     cy.get(elementId).click()
     if (value) {
@@ -46,9 +45,9 @@ export const selectOptionDropdown = (form: string, key: string, value?: string) 
         // default select first option in list
         cy.get('.ant-select-dropdown :not(.ant-select-dropdown-hidden)', { timeout: 10000 }).should('be.visible').find('.ant-select-item-option').first().click({ force: true })
     }
-}
+})
 
-export const setSearchBoxValueAndSelectFirstOption = (key: string, value: string, selectListKey: string, form?: string, googleGeocoding?: boolean, clear?: boolean) => {
+Cypress.Commands.add('setSearchBoxValueAndSelectFirstOption', (key: string, value: string, selectListKey: string, form?: string, googleGeocoding?: boolean, clear?: boolean) => {
     const elementId: string = generateElementId(key, form)
     if (clear) {
         cy.get(elementId).clear().type(value).should('have.value', value)
@@ -61,36 +60,31 @@ export const setSearchBoxValueAndSelectFirstOption = (key: string, value: string
         cy.get(elementId).type('{downarrow}')
     }
     cy.get(elementId).type('{enter}')
-}
+})
 
-export const clickButton = (key: string, form?: string, switchBtn?: boolean) => {
+Cypress.Commands.add('clickButton', (key: string, form?: string, switchBtn?: boolean) => {
     const elementId: string = generateElementId(key, form)
     if (switchBtn) {
         cy.get(elementId).find('button').click()
     } else {
         cy.get(elementId).click()
     }
-}
+})
 
-export const clickDeleteButtonWithConf = (form?: string, key = 'delete-btn') => {
+Cypress.Commands.add('clickDeleteButtonWithConf', (form?: string, key = 'delete-btn') => {
     cy.clickButton(key, form)
     // get popover conf box and click confirmation button
     cy.get('.ant-popover-inner-content', { timeout: 10000 }).should('be.visible').find('.ant-popover-buttons > :nth-child(2)').click()
-}
+})
 
-export const uploadFile = (key: string, filePath: string, form?: string) => {
+Cypress.Commands.add('uploadFile', (key: string, filePath: string, form?: string) => {
     cy.get(generateElementId(key, form)).attachFile(filePath)
-}
+})
 
-export const checkSuccessToastMessage = () => {
+Cypress.Commands.add('checkSuccessToastMessage', () => {
     cy.get('.ant-notification-notice-success .ant-notification-notice-close', { timeout: 10000 }).should('be.visible')
-}
+})
 
-export const clearDropdownSelection = (fieldName: string) => {
+Cypress.Commands.add('clearDropdownSelection', (fieldName: string) => {
     cy.get(`.ant-select[name="${fieldName}"] > span.ant-select-clear`).click({ force: true })
-}
-
-export const checkFirstCheckBox = (key: string, form?: string,) => {
-    const elementId: string = generateElementId(key, form)
-    cy.get(elementId).first().check()
-}
+})
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,6 @@ import timezonePlugin from 'dayjs/plugin/timezone'
 import isBetween from 'dayjs/plugin/isBetween'
 import minMax from 'dayjs/plugin/minMax'
 
-// cypress commands
-import {
-    apiAuth, checkFirstCheckBox,
-    checkSuccessToastMessage,
-    clearDropdownSelection,
-    clickButton, clickDeleteButtonWithConf,
-    selectOptionDropdown, setInputValue,
-    setSearchBoxValueAndSelectFirstOption, uploadFile
-} from './commands/cypressCommands'
-
 // dayjs extended plugins
 dayjs.extend(isBetween)
 dayjs.extend(utcPlugin)
@@ -38,22 +28,3 @@ export { default as TextareaField } from './atoms/TextareaField'
 export { default as TimeField } from './atoms/TimeField'
 export { default as TimeRangeField } from './atoms/TimeRangeField'
 export { default as ToggleField } from './atoms/ToggleField'
-
-// add all custom test commands for cypress
-const initializeCustomCommands = () => {
-    Cypress.Commands.addAll({
-        apiAuth,
-        setInputValue,
-        selectOptionDropdown,
-        setSearchBoxValueAndSelectFirstOption,
-        clickButton,
-        clickDeleteButtonWithConf,
-        uploadFile,
-        checkSuccessToastMessage,
-        clearDropdownSelection,
-        checkFirstCheckBox
-    })
-}
-
-// init all commands
-initializeCustomCommands()

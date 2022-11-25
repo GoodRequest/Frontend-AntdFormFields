a857e5c713cf80f91e89c472ac74eb82caefeaca
Daniel Hlavaty <danielhlavaty@GoodReqtsiPhone.localdomain>
2022-11-25 16:02:34 +0100

    fix v5 antd oopsie

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import '../src/styles/tailwind.css'
 import '../src/styles/_global.sass'
 import '../src/styles/_theme.sass'
-import 'antd/dist/antd.css'
+import 'antd/dist/antd'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

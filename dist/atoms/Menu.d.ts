2503da7d0343185d6b243cd07ed4856541766e0d
Juraj Chripko <juraj770@gmail.com>
2023-01-19 16:40:06 +0100

    added dist to .gitignore and removed files from dist folder

--- dist/atoms/Menu.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import { MenuProps } from 'antd/lib/menu';
-declare type Props = MenuProps;
-declare const Menu: (props: Props) => JSX.Element;
-export default Menu;

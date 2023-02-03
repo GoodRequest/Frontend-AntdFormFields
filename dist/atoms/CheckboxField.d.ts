fff32c7df8ca4359d746985776dc6cdb1c9a7c7a
HaluskaRoman <roman.haluska@goodrequest.com>
2023-02-03 12:08:50 +0100

    delete dist folder

--- dist/atoms/CheckboxField.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { WrappedFieldProps } from 'redux-form';
-import { InputProps } from 'antd/lib/input';
-import { FormItemLabelProps } from 'antd/lib/form/FormItemLabel';
-type Props = WrappedFieldProps & InputProps & FormItemLabelProps;
-declare const CheckboxField: (props: Props) => JSX.Element;
-export default CheckboxField;
--- dist/atoms/ToggleField.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { WrappedFieldProps } from 'redux-form';
-import { InputProps } from 'antd/lib/input';
-import { FormItemLabelProps } from 'antd/lib/form/FormItemLabel';
-type Props = WrappedFieldProps & InputProps & FormItemLabelProps & {
-    options: any;
-};
-declare const ToggleField: (props: Props) => JSX.Element;
-export default ToggleField;
